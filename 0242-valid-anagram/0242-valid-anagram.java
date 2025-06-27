bcc07484c1c7d9337702adeff8c1779846e383ff
Tanuja Dinesh Gouda <tanujadgg2004@gmail.com>
2025-06-27 14:00:57 +0530

    Time: 7 ms (35.45%), Space: 44.4 MB (54%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -2,6 +2,9 @@ class Solution {
     public boolean isAnagram(String s, String t) {
        char[] ch1 = s.toCharArray();
        char[] ch2 = t.toCharArray();
+       if(s.length() != t.length()){
+        return false;
+       }
        Arrays.sort(ch1);
        Arrays.sort(ch2);
        return Arrays.equals(ch1,ch2); 

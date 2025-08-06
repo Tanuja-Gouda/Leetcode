ef09929d031ddfefdbc43de3d0af3278f969ea7d
Tanuja Dinesh Gouda <tanujadgg2004@gmail.com>
2025-08-06 21:16:45 +0530

    Time: 0 ms (100%), Space: 40.5 MB (85.94%) - LeetHub

--- 0202-happy-number/0202-happy-number.java
+++ 0202-happy-number/0202-happy-number.java
@@ -1,15 +1,38 @@
 class Solution {
-    public boolean isHappy(int n) {
-        if(n==1 || n==7) return true;
-        else if(n<10) return false;
-        else{
-            int sum=0;
-            while(n>0){
-                int temp=n%10;
-                sum+= temp*temp;
-                n=n/10;
-            }
-            return isHappy(sum);
+
+    static int findSquare(int num){
+        int ans = 0;
+        while(num > 0){
+            int rem = num % 10;
+            ans += rem*rem;
+            num /= 10;
         }
+        return ans;
+    }
+
+    public boolean isHappy(int n) {
+        // if(n==1 || n==7) return true;
+        // else if(n<10) return false;
+        // else{
+        //     int sum=0;
+        //     while(n>0){
+        //         int temp=n%10;
+        //         sum+= temp*temp;
+        //         n=n/10;
+        //     }
+        //     return isHappy(sum);
+        // }
+
+        int fast = n;
+        int slow = n;
+        do{
+            slow = findSquare(slow);
+            fast = findSquare(findSquare(fast));
+        }while(slow != fast);
+
+        if(slow == 1){
+            return true;
+        }
+        return false;
     }
 }

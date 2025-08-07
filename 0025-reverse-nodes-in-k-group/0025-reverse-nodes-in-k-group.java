d6efa78e2e3e8471ff9b90cc7187c16c87a58da5
Tanuja Dinesh Gouda <tanujadgg2004@gmail.com>
2025-08-07 07:10:25 +0530

    Time: 1 ms (30.02%), Space: 44.7 MB (21.68%) - LeetHub

--- 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.java
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.java
@@ -10,41 +10,78 @@
  */
 class Solution {
     public ListNode reverseKGroup(ListNode head, int k) {
-        if(k<=1 || head == null){
+        // if(k<=1 || head == null){
+        //     return head;
+        // }
+
+        // ListNode prev = null;
+        // ListNode cur = head;
+        // while(true){
+        //     ListNode node = cur;
+        //     for (int i = 1; i < k && node != null; i++) {
+        //         node = node.next;
+        //     }
+        //     if (node == null) break;
+
+
+        //     ListNode last = prev;
+        //     ListNode newEnd = cur;
+        //     ListNode next = cur.next;
+        //     for(int i=0;cur!=null && i<k;i++){
+        //         cur.next = prev;
+        //         prev = cur;
+        //         cur = next;
+        //         if(cur != null){
+        //             next = cur.next;
+        //         }
+        //     }
+        //     if(last != null){
+        //         last.next = prev;
+        //     }else{
+        //         head = prev;
+        //     }
+        //     newEnd.next = cur;
+
+
+        //     prev = newEnd;
+        // }
+        // return head;
+
+        if (k <= 1 || head == null) {
             return head;
         }
 
-        ListNode prev = null;
-        ListNode cur = head;
-        while(true){
-            ListNode node = cur;
-            for (int i = 1; i < k && node != null; i++) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode prev = dummy;
+
+        while (true) {
+            ListNode node = prev;
+            // Check if there are k nodes left to reverse
+            for (int i = 0; i < k && node != null; i++) {
                 node = node.next;
             }
             if (node == null) break;
 
+            ListNode cur = prev.next;
+            ListNode next = null;
+            ListNode last = cur;
 
-            ListNode last = prev;
-            ListNode newEnd = cur;
-            ListNode next = cur.next;
-            for(int i=0;cur!=null && i<k;i++){
-                cur.next = prev;
-                prev = cur;
+            // Reverse k nodes
+            ListNode prevNode = null;
+            for (int i = 0; i < k; i++) {
+                next = cur.next;
+                cur.next = prevNode;
+                prevNode = cur;
                 cur = next;
-                if(cur != null){
-                    next = cur.next;
-                }
             }
-            if(last != null){
-                last.next = prev;
-            }else{
-                head = prev;
-            }
-            newEnd.next = cur;
 
-
-            prev = newEnd;
+            // Connect with previous part
+            prev.next = prevNode;
+            last.next = cur;
+            prev = last;
         }
-        return head;
+
+        return dummy.next;
     }
 }

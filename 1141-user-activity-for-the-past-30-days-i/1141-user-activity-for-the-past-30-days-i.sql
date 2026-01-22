fc0c9a1b53fac23b718d462a2b4b98994f1096c3
Tanuja Dinesh Gouda <tanujadgg2004@gmail.com>
2026-01-23 04:37:59 +0530

    Time: 534 ms (16.95%), Space: 0B (100%) - LeetHub

--- 1141-user-activity-for-the-past-30-days-i/1141-user-activity-for-the-past-30-days-i.sql
+++ 1141-user-activity-for-the-past-30-days-i/1141-user-activity-for-the-past-30-days-i.sql
@@ -3,7 +3,7 @@ SELECT activity_date AS 'day', COUNT(DISTINCT user_id) AS 'active_users'
 -- FROM (SELECT user_id,activity_date FROM Activity WHERE activity_date > '2019-06-27' AND activity_date <= '2019-07-27') t2
 -- GROUP BY t2.activity_date;
 FROM Activity
-WHERE activity_date > '2019-06-27' AND activity_date <= '2019-07-27'
+WHERE activity_date BETWEEN '2019-06-28' AND '2019-07-27'
 GROUP BY activity_date;
 
 

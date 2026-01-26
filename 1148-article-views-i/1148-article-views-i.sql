6df265384d7a91b8fa7148683b1469507a72a08c
Tanuja Dinesh Gouda <tanujadgg2004@gmail.com>
2026-01-26 19:37:32 +0530

    Time: 422 ms (34.36%), Space: 0B (100%) - LeetHub

--- 1148-article-views-i/1148-article-views-i.sql
+++ 1148-article-views-i/1148-article-views-i.sql
@@ -1,6 +1,23 @@
-# Write your MySQL query statement below
-SELECT author_id as id
-FROM Views
-WHERE author_id = viewer_id
-GROUP BY author_id
+-- # Write your MySQL query statement below
+
+select DISTINCT author_id as id
+from Views
+where author_id=viewer_id
 ORDER BY id;
+
+
+
+
+
+
+
+
+
+
+
+
+-- SELECT DISTINCT author_id as id
+-- FROM Views
+-- WHERE author_id = viewer_id
+-- -- GROUP BY author_id
+-- ORDER BY id;

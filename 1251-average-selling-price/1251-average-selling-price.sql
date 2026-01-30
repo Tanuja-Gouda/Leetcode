6b034e1774bcfc90d0b78afe03d8af5757a97420
Tanuja Dinesh Gouda <tanujadgg2004@gmail.com>
2026-01-30 09:57:13 +0530

    Time: 672 ms (96.09%), Space: 0B (100%) - LeetHub

--- 1251-average-selling-price/1251-average-selling-price.sql
+++ 1251-average-selling-price/1251-average-selling-price.sql
@@ -1,5 +1,39 @@
 # Write your MySQL query statement below
-SELECT p.product_id, IFNULL(ROUND(SUM(p.price * us.units)/SUM(us.units),2),0) as average_price
-FROM Prices as p LEFT JOIN UnitsSold as us ON p.product_id=us.product_id
-     AND us.purchase_date BETWEEN p.start_date and p.end_date
-GROUP BY p.product_id;
+
+select p.product_id,ifnull(round(sum(p.price*u.units)/sum(u.units),2),0) average_price
+from Prices p 
+     left join UnitsSold u
+     on p.product_id=u.product_id and u.purchase_date between p.start_date and p.end_date
+group by p.product_id;
+
+
+
+
+
+
+
+
+
+
+
+
+
+-- SELECT p.product_id, IFNULL(ROUND(SUM(p.price * us.units)/SUM(us.units),2),0) as average_price
+-- FROM Prices as p LEFT JOIN UnitsSold as us ON p.product_id=us.product_id
+--     --  AND us.purchase_date BETWEEN p.start_date and p.end_date
+-- WHERE us.purchase_date BETWEEN p.start_date and p.end_date
+-- GROUP BY p.product_id;
+
+-- you cant use the where condition if udes then it works like INNNER JOIN  
+
+
+
+
+
+
+
+
+
+
+
+

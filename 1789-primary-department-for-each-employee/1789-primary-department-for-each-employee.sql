565ff25aabdbd17b1050a7ab49079057ab56392c
Tanuja Dinesh Gouda <tanujadgg2004@gmail.com>
2026-02-02 10:56:17 +0530

    Time: 543 ms (41.64%), Space: 0B (100%) - LeetHub

--- 1789-primary-department-for-each-employee/1789-primary-department-for-each-employee.sql
+++ 1789-primary-department-for-each-employee/1789-primary-department-for-each-employee.sql
@@ -1,8 +1,43 @@
 # Write your MySQL query statement below
-SELECT employee_id,department_id
-FROM Employee
-WHERE primary_flag='Y' OR 
-      employee_id IN (SELECT employee_id
-                      FROM Employee 
-                      GROUP BY employee_id
-                      HAVING COUNT(Employee_id)=1);
+
+select employee_id,department_id
+from Employee 
+where primary_flag='Y'
+      or 
+      employee_id in (select employee_id
+                      from Employee
+                      group by employee_id
+                      having count(department_id)=1);
+
+
+
+
+
+
+
+
+
+
+
+
+
+-- SELECT employee_id,department_id
+-- FROM Employee
+-- WHERE primary_flag='Y' OR 
+--       employee_id IN (SELECT employee_id
+--                       FROM Employee 
+--                       GROUP BY employee_id
+--                       HAVING COUNT(Employee_id)=1);
+
+
+
+
+
+
+
+
+
+
+
+
+                      

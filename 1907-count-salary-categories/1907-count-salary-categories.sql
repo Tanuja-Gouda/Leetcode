e0866dde5a489b31beaa5ed45fa33fda74431fa0
Tanuja Dinesh Gouda <tanujadgg2004@gmail.com>
2026-02-02 16:36:28 +0530

    Time: 1605 ms (32.99%), Space: 0B (100%) - LeetHub

--- 1907-count-salary-categories/1907-count-salary-categories.sql
+++ 1907-count-salary-categories/1907-count-salary-categories.sql
@@ -1,12 +1,38 @@
 # Write your MySQL query statement below
-SELECT 'Low Salary' AS category, COUNT(CASE WHEN income<20000 THEN 1 END) AS accounts_count
-FROM Accounts
 
-UNION ALL
-SELECT 'Average Salary' AS category, COUNT(CASE WHEN income BETWEEN 20000 AND 50000 THEN 1 END) AS accounts_count
-FROM Accounts
+select 'Low Salary' as category, ifnull(count(case when income<20000 then 1 end),0) as accounts_count
+from Accounts
 
-UNION ALL
-SELECT 'High Salary' AS category, COUNT(CASE WHEN income>50000 THEN 1 END) AS accounts_count
-FROM Accounts
+union all
+
+select 'Average Salary' as category, ifnull(count(case when income between 20000 and 50000 then 1 end),0) as accounts_count
+from Accounts
+
+union all
+
+select 'High Salary' as category, ifnull(count(case when income>50000 then 1 end),0) as account_count
+from Accounts;
+
+
+
+
+
+
+
+
+
+
+
+
+
+-- SELECT 'Low Salary' AS category, COUNT(CASE WHEN income<20000 THEN 1 END) AS accounts_count
+-- FROM Accounts
+
+-- UNION ALL
+-- SELECT 'Average Salary' AS category, COUNT(CASE WHEN income BETWEEN 20000 AND 50000 THEN 1 END) AS accounts_count
+-- FROM Accounts
+
+-- UNION ALL
+-- SELECT 'High Salary' AS category, COUNT(CASE WHEN income>50000 THEN 1 END) AS accounts_count
+-- FROM Accounts
 

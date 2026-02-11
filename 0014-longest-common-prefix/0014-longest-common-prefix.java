2043c66ce79c4b0be6d1bf3be3f3e652b115c4ea
Tanuja Dinesh Gouda <tanujadgg2004@gmail.com>
2026-02-11 20:22:46 +0530

    Time: 0 ms (100%), Space: 43.3 MB (47.94%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -1,22 +1,56 @@
 class Solution {
     public String longestCommonPrefix(String[] strs) {
-        if(strs == null || strs.length == 0){
+        if(strs==null || strs.length==0){
             return "";
         }
 
-        String pref = strs[0];
-        int prefLen = pref.length();
+        String pref=strs[0];
+        int prefLen=pref.length();
 
-        for(int i = 1; i< strs.length; i++){
-            String s = strs[i];
-            while(prefLen > s.length() || !pref.equals(s.substring(0,prefLen))){
+        for(int i=1;i<strs.length;i++){
+            String s=strs[i];
+            while(prefLen>s.length() || !pref.equals(s.substring(0,prefLen))){
                 prefLen--;
-                if(prefLen == 0){
+                if(prefLen==0){
                     return "";
                 }
-            pref = pref.substring(0,prefLen);
+            pref=pref.substring(0,prefLen);
             }
         }
         return pref;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // if(strs == null || strs.length == 0){
+        //     return "";
+        // }
+
+        // String pref = strs[0];
+        // int prefLen = pref.length();
+
+        // for(int i = 1; i< strs.length; i++){
+        //     String s = strs[i];
+        //     while(prefLen > s.length() || !pref.equals(s.substring(0,prefLen))){
+        //         prefLen--;
+        //         if(prefLen == 0){
+        //             return "";
+        //         }
+        //     pref = pref.substring(0,prefLen);
+        //     }
+        // }
+        // return pref;
     }
 }

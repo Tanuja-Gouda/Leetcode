54a9e1b2f09f46f8a289136501d6d279084a339e
Tanuja Dinesh Gouda <tanujadgg2004@gmail.com>
2026-02-12 22:45:10 +0530

    Time: 0 ms (100%), Space: 48.6 MB (11.13%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,49 +1,100 @@
 class Solution {
 
-     static int findFirstIndex(int[] nums,int x){
-        int low = 0;
-        int high = nums.length-1;
-        int ans = -1;
-        while(low<=high){
-            int mid=low+(high-low)/2;
-            if(nums[mid]==x){
+
+
+    //     int low = 0;
+    //     int high = nums.length-1;
+    //     int ans = -1;
+    //     while(low<=high){
+    //         int mid=low+(high-low)/2;
+    //         if(nums[mid]==x){
+    //             ans=mid;
+    //             high=mid-1;
+    //         }
+    //         else if(nums[mid]<x){
+    //             low=mid+1;
+    //         }else{
+    //             high = mid-1;
+    //         }
+    //     }
+    //     return ans;
+    // }
+
+    //  static int findLastIndex(int[] nums,int x){
+    //     int low = 0;
+    //     int high = nums.length-1;
+    //     int ans = -1;
+    //     while(low<=high){
+    //         int mid=low+(high-low)/2;
+    //         if(nums[mid]==x){
+    //             ans=mid;
+    //             low=mid+1;
+    //         }
+    //         else if(nums[mid]<x){
+    //             low=mid+1;
+    //         }else{
+    //             high = mid-1;
+    //         }
+    //     }
+    //     return ans;
+    // }
+
+    static int findFirstIndex(int[] nums,int target){
+        int ans=-1;
+        int left=0;
+        int right=nums.length-1;
+
+        while(left<=right){
+            int mid=(left+right)/2;
+
+            if(nums[mid]==target){
                 ans=mid;
+                right=mid-1;
+            }else if(nums[mid]>target){
+                right=mid-1;
+            }else{
+                left=mid+1;
+            }
+        }
+        return ans;
+    }
+
+    static int findLastIndex(int[] nums,int target){
+        int low=0;
+        int high=nums.length-1;
+        int ans=-1;
+
+        while(low<=high){
+            int mid=(low+high)/2;
+
+            if(nums[mid]==target){
+                ans=mid;
+                low=mid+1;
+            }else if(nums[mid]>target){
                 high=mid-1;
-            }
-            else if(nums[mid]<x){
-                low=mid+1;
+
             }else{
-                high = mid-1;
+                low=mid+1;
             }
         }
         return ans;
     }
-
-     static int findLastIndex(int[] nums,int x){
-        int low = 0;
-        int high = nums.length-1;
-        int ans = -1;
-        while(low<=high){
-            int mid=low+(high-low)/2;
-            if(nums[mid]==x){
-                ans=mid;
-                low=mid+1;
-            }
-            else if(nums[mid]<x){
-                low=mid+1;
-            }else{
-                high = mid-1;
-            }
-        }
-        return ans;
-    }
-
 
     public int[] searchRange(int[] nums, int target) {
-        int first =findFirstIndex(nums,target);
-        int last = findLastIndex(nums,target);
+        int firstIndex=findFirstIndex(nums,target);
+        int lastIndex=findLastIndex(nums,target);
 
-        return new int[] {first,last};
+        return new int[] {firstIndex,lastIndex};
+
+
+
+
+
+
+    //     int first =findFirstIndex(nums,target);
+    //     int last = findLastIndex(nums,target);
+
+    //     return new int[] {first,last};
 
     }
 }

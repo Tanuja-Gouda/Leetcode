1e7514325805e17447be637f3d7b8abe5756dcb7
Tanuja Dinesh Gouda <tanujadgg2004@gmail.com>
2026-06-05 21:23:42 +0530

    Time: 0 ms (100%), Space: 48.1 MB (32.96%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.java
@@ -0,0 +1,26 @@
+class Solution {
+    public double myPow(double x, int n) {
+        long power = n;
+
+        if (power < 0) {
+            x = 1 / x;
+            power = -power;
+        }
+
+        return fastPow(x, power);
+    }
+
+    private double fastPow(double x, long n) {
+        if (n == 0) {
+            return 1.0;
+        }
+
+        double half = fastPow(x, n / 2);
+
+        if (n % 2 == 0) {
+            return half * half;
+        }
+
+        return half * half * x;
+    }
+}

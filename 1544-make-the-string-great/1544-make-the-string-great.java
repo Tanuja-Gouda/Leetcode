1684ef4527f9ad893128dc9266478c1365fe9ec4
Tanuja Dinesh Gouda <tanujadgg2004@gmail.com>
2026-06-06 22:01:18 +0530

    Time: 3 ms (61.7%), Space: 44 MB (24.82%) - LeetHub

---
+++ 1544-make-the-string-great/1544-make-the-string-great.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String makeGood(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char ch : s.toCharArray()) {
+            if (!stack.isEmpty() &&
+                Math.abs(stack.peek() - ch) == 32) {
+                stack.pop();
+            } else {
+                stack.push(ch);
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        while (!stack.isEmpty()) {
+            sb.append(stack.pop());
+        }
+
+        return sb.reverse().toString();
+    }
+}

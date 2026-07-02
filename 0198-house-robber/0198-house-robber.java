2d0c840d09d6fda91c1157a117240b837821609c
Tanuja Dinesh Gouda <tanujadgg2004@gmail.com>
2026-07-02 20:21:18 +0530

    Time: 0 ms (100%), Space: 42.6 MB (87.53%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,23 +1,37 @@
 class Solution {
     public int rob(int[] nums) {
         int n=nums.length;
-        int[] dp=new int[n];
-        Arrays.fill(dp,-1);
+        // int[] dp=new int[n];
+        // Arrays.fill(dp,-1);
 
-        dp[0]=nums[0];
-        for(int i=1;i<n;i++){
-            int notTake = 0+dp[i-1];
-            int take=nums[i];
-            if(i>1){
-              take += dp[i-2];
-            }
+        // dp[0]=nums[0];
+        // for(int i=1;i<n;i++){
+        //     int notTake = 0+dp[i-1];
+        //     int take=nums[i];
+        //     if(i>1){
+        //       take += dp[i-2];
+        //     }
 
-            dp[i]=Math.max(take,notTake);
-        }
+        //     dp[i]=Math.max(take,notTake);
+        // }
+        // return dp[n-1];
 
 
         // return findMoney(nums,n-1,dp);
-        return dp[n-1];
+
+        if(n==1){
+            return nums[0];
+        }
+
+        int prev2=nums[0];
+        int prev1=Math.max(nums[0],nums[1]);
+
+        for(int i=2;i<n;i++){
+            int max=Math.max(nums[i]+prev2 , prev1);
+            prev2=prev1;
+            prev1=max;
+        }
+        return prev1;
     }
 
     private static int findMoney(int[] nums,int n,int[] dp){

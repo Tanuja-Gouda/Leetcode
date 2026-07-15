23651722066f87a3b489c42006119d8d3590d4f0
Tanuja Dinesh Gouda <tanujadgg2004@gmail.com>
2026-07-15 17:26:53 +0530

    Time: 0 ms (100%), Space: 42.6 MB (74.96%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,51 +1,23 @@
 class Solution {
+    int[] dp;
     public int rob(int[] nums) {
-        int n=nums.length;
-        // int[] dp=new int[n];
-        // Arrays.fill(dp,-1);
-
-        // dp[0]=nums[0];
-        // for(int i=1;i<n;i++){
-        //     int notTake = 0+dp[i-1];
-        //     int take=nums[i];
-        //     if(i>1){
-        //       take += dp[i-2];
-        //     }
-
-        //     dp[i]=Math.max(take,notTake);
-        // }
-        // return dp[n-1];
-
-
-        // return findMoney(nums,n-1,dp);
-
-        if(n==1){
-            return nums[0];
-        }
-
-        int prev2=nums[0];
-        int prev1=Math.max(nums[0],nums[1]);
-
-        for(int i=2;i<n;i++){
-            int max=Math.max(nums[i]+prev2 , prev1);
-            prev2=prev1;
-            prev1=max;
-        }
-        return prev1;
+        dp=new int[nums.length+1];
+        Arrays.fill(dp,-1);
+        return maxProfit(nums,0);
     }
 
-    private static int findMoney(int[] nums,int n,int[] dp){
-        if(n<0){
+    private int maxProfit(int[] nums,int ind){
+        if(ind>=nums.length){
             return 0;
         }
 
-        if(dp[n] != -1){
-            return dp[n];
+        if(dp[ind] != -1){
+            return dp[ind];
         }
 
-        int take=nums[n] + findMoney(nums,n-2,dp);
-        int notTake = 0+findMoney(nums,n-1,dp);
+        int pick=nums[ind]+maxProfit(nums,ind+2);
+        int notPick=0+maxProfit(nums,ind+1);
 
-        return dp[n]=Math.max(take,notTake);
+        return dp[ind] = Math.max(pick,notPick);
     }
 }

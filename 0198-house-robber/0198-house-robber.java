97eff176783161701efa3d11bf89dc274f662598
Tanuja Dinesh Gouda <tanujadgg2004@gmail.com>
2026-07-15 17:41:55 +0530

    Time: 0 ms (100%), Space: 42.9 MB (18.66%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -2,22 +2,39 @@ class Solution {
     int[] dp;
     public int rob(int[] nums) {
         dp=new int[nums.length+1];
-        Arrays.fill(dp,-1);
-        return maxProfit(nums,0);
-    }
 
-    private int maxProfit(int[] nums,int ind){
-        if(ind>=nums.length){
-            return 0;
+        dp[0]=nums[0];
+        
+        for(int i=1;i<nums.length;i++){
+            int pick=nums[i];
+            int notPick=dp[i-1];
+            if(i>1){
+                pick+=dp[i-2];
+            }
+
+            dp[i]=Math.max(pick,notPick);
         }
 
-        if(dp[ind] != -1){
-            return dp[ind];
-        }
+        return dp[nums.length-1];
 
-        int pick=nums[ind]+maxProfit(nums,ind+2);
-        int notPick=0+maxProfit(nums,ind+1);
 
-        return dp[ind] = Math.max(pick,notPick);
+
+        // Arrays.fill(dp,-1);
+        // return maxProfit(nums,0);
     }
+
+    // private int maxProfit(int[] nums,int ind){
+    //     if(ind>=nums.length){
+    //         return 0;
+    //     }
+
+    //     if(dp[ind] != -1){
+    //         return dp[ind];
+    //     }
+
+    //     int pick=nums[ind]+maxProfit(nums,ind+2);
+    //     int notPick=0+maxProfit(nums,ind+1);
+
+    //     return dp[ind] = Math.max(pick,notPick);
+    // }
 }

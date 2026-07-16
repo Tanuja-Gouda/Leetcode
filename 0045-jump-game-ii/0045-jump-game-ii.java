364eb34f26c96b687b58c5a9d5bac1264e68a6af
Tanuja Dinesh Gouda <tanujadgg2004@gmail.com>
2026-07-16 20:10:03 +0530

    Time: 116 ms (7.45%), Space: 47.5 MB (8.66%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -1,24 +1,47 @@
 class Solution {
 
+    int[] dp;
     public int jump(int[] nums) {
+        dp=new int[nums.length];
+        Arrays.fill(dp,-1);
+        return findJump(nums,0);
+    }
 
-        int jumps = 0;
-        int currentEnd = 0;
-        int farthest = 0;
+    private int findJump(int[] nums,int ind){
+        if(ind==nums.length-1){
+            return 0;
+        }
 
-        for (int i = 0; i < nums.length - 1; i++) {
+        if(dp[ind] != -1){
+            return dp[ind];
+        }        
 
-            farthest = Math.max(
-                    farthest,
-                    i + nums[i]);
-
-            if (i == currentEnd) {
-
-                jumps++;
-                currentEnd = farthest;
+        int min=Integer.MAX_VALUE;
+        for(int i=ind+1;i<=Math.min(nums.length-1,ind+nums[ind]);i++){
+            int steps=findJump(nums,i);
+            if (steps != Integer.MAX_VALUE){
+                min = Math.min(min, 1 + steps);
             }
         }
 
-        return jumps;
+        return dp[ind] = min;
     }
 }
+        // int jumps = 0;
+        // int currentEnd = 0;
+        // int farthest = 0;
+
+        // for (int i = 0; i < nums.length - 1; i++) {
+
+        //     farthest = Math.max(
+        //             farthest,
+        //             i + nums[i]);
+
+        //     if (i == currentEnd) {
+
+        //         jumps++;
+        //         currentEnd = farthest;
+        //     }
+        // }
+
+        // return jumps;

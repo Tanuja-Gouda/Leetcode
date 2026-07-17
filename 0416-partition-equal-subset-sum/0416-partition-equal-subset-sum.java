1e7b6bfc62eeefadad771cdf348077d3e85f629c
Tanuja Dinesh Gouda <tanujadgg2004@gmail.com>
2026-07-17 10:19:52 +0530

    Time: 43 ms (60.61%), Space: 47.6 MB (49.37%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,5 +1,5 @@
 class Solution {
-    Boolean[][] dp;
+    boolean[][] dp;
     public boolean canPartition(int[] nums) {
         int n=nums.length;
         int totalSum=0;
@@ -7,36 +7,59 @@ class Solution {
             totalSum += num;
         }
 
-        dp=new Boolean[n+1][totalSum+1];
         if(totalSum%2 == 1){
             return false;
         }
 
         int target=totalSum/2;
+        dp=new boolean[n][target+1];
 
-        return findTarget(nums,target,0);
+        for(int i=0;i<nums.length;i++){
+            dp[i][0]=true;
+        }
+
+        if(nums[0]<=target){
+            dp[0][nums[0]]=true;
+        }
+
+        for(int i=1;i<nums.length;i++){
+            for(int j=1;j<=target;j++){
+                boolean take=false;
+                if(nums[i]<=j){
+                    take=dp[i-1][j-nums[i]];
+                }
+                boolean notTake=dp[i-1][j];
+                dp[i][j]=take || notTake;
+            }
+        }
+
+        return dp[n-1][target];
+
+
+
+        // return findTarget(nums,target,0);
     }
 
-    public boolean findTarget(int[] nums, int target,int ind){
-        if(target==0){
-            return true;
-        }
-        if(ind==nums.length){
-            return false;
-        }
+    // public boolean findTarget(int[] nums, int target,int ind){
+    //     if(target==0){
+    //         return true;
+    //     }
+    //     if(ind==nums.length){
+    //         return false;
+    //     }
 
-        if(dp[ind][target] != null){
-            return dp[ind][target];
-        }
+    //     if(dp[ind][target] != null){
+    //         return dp[ind][target];
+    //     }
 
-        boolean take=false;
+    //     boolean take=false;
 
-        if(nums[ind]<=target){
-            take=findTarget(nums,target-nums[ind],ind+1);
-        }
+    //     if(nums[ind]<=target){
+    //         take=findTarget(nums,target-nums[ind],ind+1);
+    //     }
         
-        boolean notTake=findTarget(nums,target,ind+1);
+    //     boolean notTake=findTarget(nums,target,ind+1);
 
-        return dp[ind][target] = take || notTake;
-    }
+    //     return dp[ind][target] = take || notTake;
+    // }
 }

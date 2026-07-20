97854baa30caab8840758ae408858bed857d28f2
Tanuja Dinesh Gouda <tanujadgg2004@gmail.com>
2026-07-20 12:44:33 +0530

    Time: 0 ms (100%), Space: 43.3 MB (89.31%) - LeetHub

---
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int n=prices.length;
+        int nextBuy=0;
+        int nextSell=0;
+        int nextBuy2=0;
+        int profit=0;
+
+        for(int i=n-1;i>=0;i--){
+            int curBuy=0;
+            int curSell=0;
+            for(int buy=1;buy>=0;buy--){
+                if(buy==1){
+                    curBuy=Math.max(nextBuy, -prices[i]+nextSell);
+                }else{
+                    curSell=Math.max(nextSell, prices[i]+nextBuy2);
+                }
+            }
+            nextBuy2=nextBuy;
+            nextBuy=curBuy;
+            nextSell=curSell;
+        }
+        return nextBuy;
+    }
+}

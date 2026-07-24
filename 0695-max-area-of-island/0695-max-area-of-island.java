9f94cc95dacbbb1d199a011eec67f5384db150f7
Tanuja Dinesh Gouda <tanujadgg2004@gmail.com>
2026-07-24 11:27:54 +0530

    Time: 2 ms (64.24%), Space: 46.8 MB (15.62%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.java
+++ 0695-max-area-of-island/0695-max-area-of-island.java
@@ -1,37 +1,39 @@
 class Solution {
 
     public int maxAreaOfIsland(int[][] grid) {
-        int maxArea = 0;
+        int m = grid.length;
+        int n = grid[0].length;
 
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[0].length; j++) {
+        int max=0;
 
-                if (grid[i][j] == 1) {
-                    maxArea = Math.max(maxArea, dfs(i, j, grid));
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j]==1){
+                    max=Math.max(max,dfs(i,j,grid));
                 }
             }
         }
-
-        return maxArea;
+        return max;
     }
 
-    private int dfs(int row, int col, int[][] grid) {
-
-        if (row < 0 || col < 0 ||
-            row >= grid.length || col >= grid[0].length ||
-            grid[row][col] == 0) {
+    public int dfs(int row,int col,int[][] grid){
+        if(row<0 || col<0 || row>grid.length-1 || col>grid[0].length-1 || grid[row][col]==0){
             return 0;
         }
 
-        grid[row][col] = 0;
+        grid[row][col]=0;
 
-        int area = 1;
+        int ans=1;
+        int[] dr={0,1,0,-1};
+        int[] dc={-1,0,1,0};
 
-        area += dfs(row + 1, col, grid); // Down
-        area += dfs(row - 1, col, grid); // Up
-        area += dfs(row, col + 1, grid); // Right
-        area += dfs(row, col - 1, grid); // Left
+        for(int i=0;i<4;i++){
+            int r=row+dr[i];
+            int c=col+dc[i];
 
-        return area;
+            ans += dfs(r,c,grid);
+        }
+        return ans;
     }
 }
+

4c8e3d5820e411a8c54720443ce19c6d24812226
Tanuja Dinesh Gouda <tanujadgg2004@gmail.com>
2026-07-24 12:03:53 +0530

    Time: 0 ms (100%), Space: 46.4 MB (26.18%) - LeetHub

--- 0841-keys-and-rooms/0841-keys-and-rooms.java
+++ 0841-keys-and-rooms/0841-keys-and-rooms.java
@@ -1,31 +1,77 @@
 class Solution {
 
     public boolean canVisitAllRooms(List<List<Integer>> rooms) {
+        int m=rooms.size();
+        boolean[] entered=new boolean[m];
 
-        boolean[] visited = new boolean[rooms.size()];
+        dfs(0,rooms,entered);
 
-        dfs(0, rooms, visited);
-
-        for (boolean room : visited) {
-            if (!room) {
+        for(int i=0;i<m;i++){
+            if(entered[i]==false){
                 return false;
             }
         }
-
         return true;
     }
 
-    private void dfs(int room,
-                     List<List<Integer>> rooms,
-                     boolean[] visited) {
+    private void dfs(int room, List<List<Integer>> rooms, boolean[] entered){
+        entered[room]=true;
 
-        visited[room] = true;
-
-        for (int key : rooms.get(room)) {
-
-            if (!visited[key]) {
-                dfs(key, rooms, visited);
+        for(int key:rooms.get(room)){
+            if(entered[key] != true){
+                dfs(key,rooms,entered);
             }
         }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// boolean[] visited = new boolean[rooms.size()];
+
+//         dfs(0, rooms, visited);
+
+//         for (boolean room : visited) {
+//             if (!room) {
+//                 return false;
+//             }
+//         }
+
+//         return true;
+//     }
+
+//     private void dfs(int room,
+//                      List<List<Integer>> rooms,
+//                      boolean[] visited) {
+
+//         visited[room] = true;
+
+//         for (int key : rooms.get(room)) {
+
+//             if (!visited[key]) {
+//                 dfs(key, rooms, visited);
+//             }
+//         }
